192db80c735b188fd83c7cd614176b6abf01f9aa
Albert <albert.lozano@bosonit.com>
2023-11-06 16:55:54 +0100

    block5-command-line-runner finished

--- block5-command-line-runner/src/main/java/com/bosonit/albert/block5commandlinerunner/Block5CommandLineRunnerApplication.java
+++ block5-command-line-runner/src/main/java/com/bosonit/albert/block5commandlinerunner/Block5CommandLineRunnerApplication.java
@@ -1,36 +1,18 @@
 package com.bosonit.albert.block5commandlinerunner;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
-import javax.annotation.PostConstruct;
-
+/**
+ * Class Block5CommandLineRunnerApplication. App's main class. Public.
+ */
 @SpringBootApplication
 public class Block5CommandLineRunnerApplication {
-
+	/**
+	 * Main method. Runs the app.
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(Block5CommandLineRunnerApplication.class, args);
 	}
-
-	@PostConstruct
-	private void postConstruct() {
-		System.out.println("Hola desde clase inicial.");
-	}
-
-	@Bean
-	CommandLineRunner secondClass() {
-		return p -> {
-			System.out.println("Hola desde clase secundaria.");
-		};
-	}
-
-	@Bean
-	CommandLineRunner thirdClass() {
-		return p -> {
-			System.out.println("Soy la tercera clase.");
-		};
-	}
-
 }
---
+++ block5-command-line-runner/src/main/java/com/bosonit/albert/block5commandlinerunner/ConfigurationClass.java
@@ -0,0 +1,53 @@
+package com.bosonit.albert.block5commandlinerunner;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import javax.annotation.PostConstruct;
+
+/**
+ * Class ConfigurationClass. Sets all necessary Beans for the app.
+ */
+@Configuration
+public class ConfigurationClass {
+    /**
+     * postConstruct method. Runs before Beans are initialized.
+     * Prints a message.
+     */
+    @PostConstruct
+    private void postConstruct() {
+        String message = "Hola desde clase inicial.";
+        System.out.println(message);
+    }
+
+    /**
+     * Bean secondClass.
+     * @return an instance of SecondClass.
+     */
+    @Bean
+    CommandLineRunner secondClass() {
+        return args -> {
+            System.out.println("Hola desde clase secundaria.");
+        };
+    }
+
+    /**
+     * Bean thirdClass.
+     * @return an instance of ThirdClass.
+     */
+    @Bean
+    CommandLineRunner thirdClass() {
+        return args -> {
+            String param1 = "Soy la tercera clase.";
+            System.out.println(param1);
+
+            if (args.length >= 2) {
+                String param2 = args[0];
+                String param3 = args[1];
+                System.out.println(param2);
+                System.out.println(param3);
+            }
+        };
+    }
+
+}
