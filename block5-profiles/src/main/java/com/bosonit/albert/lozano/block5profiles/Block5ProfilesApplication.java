ee513c15e38da6b93343c938ea24888d3a5fa36b
Albert <albert.lozano@bosonit.com>
2023-11-07 16:38:10 +0100

    starting block5-profiles

---
+++ block5-profiles/src/main/java/com/bosonit/albert/lozano/block5profiles/Block5ProfilesApplication.java
@@ -0,0 +1,13 @@
+package com.bosonit.albert.lozano.block5profiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Block5ProfilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Block5ProfilesApplication.class, args);
+	}
+
+}
---
+++ block5-profiles/src/test/java/com/bosonit/albert/lozano/block5profiles/Block5ProfilesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bosonit.albert.lozano.block5profiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Block5ProfilesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
