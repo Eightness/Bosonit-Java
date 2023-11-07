8173a9ecb1c50b09f71e79c8510e03cbf7f1b28a
Albert <albert.lozano@bosonit.com>
2023-11-07 17:58:07 +0100

    block5-profiles finished

--- block5-profiles/src/main/java/com/bosonit/albert/lozano/block5profiles/Block5ProfilesApplication.java
+++ block5-profiles/src/main/java/com/bosonit/albert/lozano/block5profiles/Block5ProfilesApplication.java
@@ -3,9 +3,16 @@ package com.bosonit.albert.lozano.block5profiles;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Class Block5ProfilesApplication. App's main class.
+ */
 @SpringBootApplication
 public class Block5ProfilesApplication {
 
+	/**
+	 * main method to run the app.
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(Block5ProfilesApplication.class, args);
 	}
