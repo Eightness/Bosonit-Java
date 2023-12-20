4a15d9c851f6202b18247949716cf338347f4b6d
Albert <albert.lozano@bosonit.com>
2023-12-20 16:30:21 +0100

    block7-crud-validation first commit

---
+++ block7-crud-validation/src/test/java/com/bosonit/block7crudvalidation/Block7CrudValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bosonit.block7crudvalidation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Block7CrudValidationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
