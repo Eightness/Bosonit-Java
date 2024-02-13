69a10c5510bf5cb52278c1844c0e679bbdb1ec48
Albert <albertlb08@gmail.com>
2024-02-13 18:48:47 +0100

    block 10 first commit

---
+++ block10-dockerize-app/block10-dockerize-app/src/main/java/com/bosonit/block10dockerizeapp/Block10DockerizeAppApplication.java
@@ -0,0 +1,13 @@
+package com.bosonit.block10dockerizeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Block10DockerizeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Block10DockerizeAppApplication.class, args);
+	}
+
+}
---
+++ block10-dockerize-app/block10-dockerize-app/src/test/java/com/bosonit/block10dockerizeapp/Block10DockerizeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bosonit.block10dockerizeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Block10DockerizeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
