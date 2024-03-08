97d0501ed3078bc05507dce2042f65814a0b88c1
Albert <albert.lozano@bosonit.com>
2024-03-08 10:08:35 +0100

    block7-crud-validation CORS implementation

---
+++ block7-crud-validation/src/main/java/com/bosonit/block7crudvalidation/CORSConfig.java
@@ -0,0 +1,32 @@
+/**
+ * @author Albert Lozano Blasco
+ * @version 11.0
+ */
+
+package com.bosonit.block7crudvalidation;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * CORSConfig class. Setting CORS (Cross-Origin Resources Sharing) configuration.
+ */
+@Configuration
+public class CORSConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.addAllowedOrigin("*");
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
